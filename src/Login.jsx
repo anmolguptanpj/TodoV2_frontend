2d5b2bf5e8e1c0b039171518aeab66d929b0d754
anmolguptanpj <anmolguptanpj282@gmail.com>
2025-11-05 01:06:38 +0530

    feat: enhance login component with token handling and form submission

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import  {Link,useNavigate} from 'react-router-dom'
-function login() {
+function Login() {
   const navigate = useNavigate()
+
+  useEffect(()=>{
+    const accessToken = localStorage.getItem('accessToken');
+    if(accessToken){
+      navigate('/todos')
+    }
+  },[navigate])
 
 
   const[formData,setFormData]=useState({
@@ -26,15 +33,19 @@ function login() {
         body:JSON.stringify(formData)
       })
 
-      const data = response.json()
-      if(response.okay){
-        localStorage.set('token',data.token)
+      const data = await response.json()
+      if(response.ok){
+        localStorage.setItem('accessToken',data.accessToken)
         setMessage("Login Succesfful! Redirecting......")
         console.log('Server response:',data)
+        setTimeout(()=>{navigate('/todos')})
+      } else {
+        setMessage(data.message||"Login failed")
       }
 
-    }catch{
-
+    }catch(error){
+console.error('Error:',error);
+        setMessage('Something went wrong. Please try again later.');
     }
 
   }
@@ -42,9 +53,9 @@ function login() {
   return (
    <div>
             <h2>Login Page</h2>
-            <form onChange={submit}>
-                <input type = "text" placeholder="Username" name="identifier" value={formData.identifier}/>
-                <input type = "password" placeholder="Password" name="password" value={formData.password} />
+            <form onSubmit={handleSubmit}>
+                <input type = "text" placeholder="Username" name="identifier" value={formData.identifier} onChange={handlechange}/>
+                <input type = "password" placeholder="Password" name="password" value={formData.password} onChange={handlechange} />
                 <button type="submit"> Login </button>
             </form>
             <Link to="/">Back to Home</Link>
@@ -52,4 +63,4 @@ function login() {
   )
 }
 
-export default login
+export default Login;
